7d21051e38c67d963b835e1c075a7552ba388c28
skycode <sikiruyaya1@gmail.com>
2024-08-01 00:43:06 +0100

    constant variable initialization

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "CozyChain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Cozy Chain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "COZY"
 )
 
 var ID ids.ID
